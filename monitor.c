19a8480100dd54606bc2994db0cc6532f7a45477
Christian Guerrero <sailguerrero91@gmail.com>
2026-08-21 22:22:39 +0200

    Corrected assingacion to stop in case of eat limit

--- monitor.c
+++ monitor.c
@@ -6,7 +6,7 @@
 /*   By: chguerre <chguerre@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/20 01:24:28 by chguerr           #+#    #+#             */
-/*   Updated: 2026/08/21 22:13:34 by chguerre         ###   ########.fr       */
+/*   Updated: 2026/08/21 22:20:45 by chguerre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,9 +54,13 @@ int	all_ate_enough(t_philo *philo)
 			eat_enough++;
 		i++;
 	}
-	if (eat_enough == philo->data->num_times_to_eats)
+	if (eat_enough == philo->data->num_philos)
+	{
+		pthread_mutex_lock(&philo->data->death_mutex);
+		philo->data->someone_died = 1;
+		pthread_mutex_unlock(&philo->data->death_mutex);
 		return (1);
-
+	}
 	return (0);
 }
 
@@ -72,7 +76,9 @@ void	*monitor(void *arg)
 		ft_usleep(1, philo);
 		i = 0;
 		if (philo->data->num_times_to_eats > 0 && all_ate_enough(philo))
+		{
 			break ;
+		}
 		while (i < philo->data->num_philos)
 		{
 			one_die = is_dead(&philo[i]);
